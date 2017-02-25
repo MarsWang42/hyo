ff52ebb72be1b9124c4d05cc25435586dd2c99dd
Mars Wang <marswang92@gmail.com>
2017-02-25 11:20:02 -0800

    Custom renderer and onSort function now available.

--- src/index.jsx
+++ src/index.jsx
@@ -22,76 +22,79 @@ export default class Table extends Component {
     });
   }
 
-  // Help function to sort rows according to given column
-  sortColumn(key) {
+  render() {
     const {
       sortingCol,
       sortingDirection,
       sortedRows,
     } = this.state;
-    // Figure out the current direction.
-    // If column is not select, then set direction to be asc.
-    // If it is already selected, set to be the opposite direction.
-    const dr = key !== sortingCol ? 'asc' :
-      sortingDirection === 'asc' ? 'desc' : 'asc';
-    const updatedRows = sortedRows.sort((a, b) => {
-      const attr1 = a[key];
-      const attr2 = b[key];
-      const order = !attr1 ? -1 : !attr2 ? 1 : attr1.toString().localeCompare(attr2);
-      return dr === 'asc' ? order : -order;
-    });
 
-    this.setState({
-      sortingCol: key,
-      sortingDirection: dr,
-      sortedRows: updatedRows,
-    });
-  }
-
-  // Generate headers according to definition
-  generateHeaders() {
     const { def } = this.props;
-    const { sortingCol, sortingDirection } = this.state;
-    const headers = def.map((col) => {
-      const thClassName = cn({ "sortable": col.sortable });
-      const spanClassName = cn({
-        "sort": col.sortable,
-        "sortup": col.sortable && col.key === sortingCol && sortingDirection === "asc",
-        "sortdown": col.sortable && col.key === sortingCol && sortingDirection === "desc",
+
+    // Help function to sort rows according to given column
+    const sortColumn = (col) => {
+      // Figure out the current direction.
+      // If column is not select, then set direction to be asc.
+      // If it is already selected, set to be the opposite direction.
+      const dr = col.key !== sortingCol ? 'asc' :
+        sortingDirection === 'asc' ? 'desc' : 'asc';
+      const updatedRows = sortedRows.sort((a, b) => {
+        const attr1 = a[col.key];
+        const attr2 = b[col.key];
+        const defaultOrder = !attr1 ? -1 : !attr2 ? 1 : attr1.toString().localeCompare(attr2);
+        const order = col.onSort? col.onSort(attr1, attr2) : defaultOrder;
+        return dr === 'asc' ? order : -order;
       });
-      return (<th key={col.key} className={thClassName} onClick={() => this.sortColumn(col.key)}>
-        {col.label}<span className={spanClassName} />
-      </th>);
-    });
-    return <thead><tr>{headers}</tr></thead>;
-  }
 
-  // Render each row according to data
-  generateRows() {
-    const { def } = this.props;
-    const { sortedRows } = this.state;
-    let i = 0;
-    const rows = sortedRows.map((row) => {
-      i+=1;
-      const cell = def.map(col =>
-        <td key={`hyo-cell-${col.key}-${i}`}>{ row[col.key] }</td>);
-      return <tr key={`hyo-row-${i}`}>{ cell }</tr>;
-    });
-    return <tbody>{ rows }</tbody>;
-  }
+      this.setState({
+        sortingCol: col.key,
+        sortingDirection: dr,
+        sortedRows: updatedRows,
+      });
+    };
 
-  // Generate the whole table
-  generateTable() {
-    return (
-      <table className="rwd-table">
-        { this.generateHeaders() }
-        { this.generateRows() }
-      </table>
-    );
-  }
+    // Generate headers according to definition
+    const generateHeaders = () => {
+      const headers = def.map((col) => {
+        const thClassName = cn({ sortable: col.sortable });
+        const spanClassName = cn({
+          sort: col.sortable,
+          sortup: col.sortable && col.key === sortingCol && sortingDirection === "asc",
+          sortdown: col.sortable && col.key === sortingCol && sortingDirection === "desc",
+        });
+        return (<th key={col.key} className={thClassName} onClick={() => sortColumn(col)}>
+          {col.label}<span className={spanClassName} />
+        </th>);
+      });
+      return <thead><tr>{headers}</tr></thead>;
+    };
 
-  render() {
-    return this.generateTable();
+    // Render each row according to data
+    const generateRows = () => {
+      let i = 0;
+      const rows = sortedRows.map((row) => {
+        i+=1;
+        const cell = def.map(col =>
+          (<td key={`hyo-cell-${col.key}-${i}`}>
+            { col.renderer? col.renderer(row[col.key]) : row[col.key] }
+          </td>));
+        return <tr key={`hyo-row-${i}`}>{ cell }</tr>;
+      });
+      return <tbody>{ rows }</tbody>;
+    };
+
+    // Generate the whole table
+    const generateTable = () => {
+      return (
+        <table className="rwd-table">
+          { generateHeaders() }
+          { generateRows() }
+        </table>
+      );
+    };
+
+
+    return generateTable();
   }
 }
 
@@ -101,6 +104,8 @@ Table.propTypes = {
       key: React.PropTypes.string.isRequired,
       label: React.PropTypes.string.isRequired,
       sortable: React.PropTypes.bool,
+      onSort: React.PropTypes.function,
+      renderer: React.PropTypes.function,
     })).isRequired,
   data: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
 };
